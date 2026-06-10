8ab96b346ae688687399865c642690f70cb74be1
Leonardo-Toterol <skylos.mz@gmail.com>
2026-06-09 21:07:02 -0300

    aula 09 06 2026

---
+++ Java/Interfaces 09-06-2026/src/Main.java
@@ -0,0 +1,17 @@
+
+void main() {
+
+    Carro carro = new Carro();
+    Moto moto = new Moto();
+
+    moto.ligar();
+    moto.desligar();
+    moto.empinar();
+
+    System.out.println("");
+
+    carro.ligar();
+    carro.desligar();
+    carro.ligarRadio();
+
+}
